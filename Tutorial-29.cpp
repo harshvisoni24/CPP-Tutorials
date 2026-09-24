cd84dc78b840ad44a539ee154fe52fa728543258
Harshvi Soni <harshvisoni24@gmail.com>
2026-09-24 17:28:17 +0530

    Constructor in c++
    Implement a Complex class with a constructor and printData method.

---
+++ Tutorial-29.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Complex{
+    int a,b;
+    public:
+    Complex(void);
+    void printData()
+    {
+        cout<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+Complex :: Complex(void)
+{
+    a=15;
+    b=12;
+}
+int main()
+{
+    Complex c;
+    c.printData();
+    return 0;
+}
+
+/* Characteristics of Constructors : 
+1. It should declare in public section of class.
+2. It can automatically invoked whenever object is created.
+3. It cannot return values & do not have return type.
+4. It can have default arguments.
+5. We cannot refer to their address.
+
+*/
