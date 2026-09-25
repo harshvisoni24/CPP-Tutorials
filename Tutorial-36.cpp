3f12d5dff7e0e61e5856da2eaccfcbf82b22900f
Harshvi Soni <harshvisoni24@gmail.com>
2026-09-25 05:42:13 +0530

    Introduction and type of inheritance.

---
+++ Tutorial-36.cpp
@@ -0,0 +1,12 @@
+/*What is Inheritance
+1. Concept of Reusability in C++ is supported using it.
+2. Existing Class is called as Base Class.
+3. New class that is inherited is called Derived Class.
+4. Reusing saves time and money.
+5. There are different type of inheritance in C++.
+--> Single Inheritance : Derived class with single base class.
+--> Multiple Inheritance : More than one Base class.
+--> Hierarchical Inheritance : More Derived class from single Base class.
+--> Multilevel Inheritance : Deriving a class from already derived class.
+--> Hybrid Inheritance : It is combination of Multilevel and Multiple Inheritance.
+*/
